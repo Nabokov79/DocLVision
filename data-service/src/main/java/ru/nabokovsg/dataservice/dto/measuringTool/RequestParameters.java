4e5ddd1ac32fc29fa1f4c969a10125b64f2ce801
Nabokov79 <nabokovsg@yandex.ru>
2023-07-24 21:10:27 +0300

    add dto class

---
+++ data-service/src/main/java/ru/nabokovsg/dataservice/dto/measuringTool/RequestParameters.java
@@ -0,0 +1,23 @@
+package ru.nabokovsg.dataservice.dto.measuringTool;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Setter
+@Getter
+@AllArgsConstructor
+public class RequestParameters {
+
+    private String name;
+    private String model;
+    private Integer workNumber;
+    private LocalDate manufacturing;
+    private LocalDate exploitation;
+    private Long manufacturerId;
+    private Long organizationId;
+    private Long typeId;
+    private Long employeeId;
+}
