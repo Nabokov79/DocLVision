8d21e0c04a53fe5a9a4a4b17ca158c222a289e90
NabokovSG <nabokovsg@yandex.ru>
2023-07-24 14:24:23 +0300

    without changes

--- data-service/src/main/java/ru/nabokovsg/dataservice/repository/AddressRepository.java
+++ data-service/src/main/java/ru/nabokovsg/dataservice/repository/AddressRepository.java
@@ -1,12 +1,11 @@
 package ru.nabokovsg.dataservice.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import ru.nabokovsg.dataservice.model.Address;
 
 import java.util.Set;
 
-public interface AddressRepository extends JpaRepository<Address, Long>, QuerydslPredicateExecutor<Address> {
+public interface AddressRepository extends JpaRepository<Address, Long> {
 
     Set<Address> findByCityId(Long cityId);
 }
