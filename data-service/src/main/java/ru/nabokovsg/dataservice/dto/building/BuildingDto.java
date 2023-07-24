15c5804e4f52c9985e4957137d7737e8cc649ce5
NabokovSG <nabokovsg@yandex.ru>
2023-07-24 14:30:51 +0300

    replace field address -> requisites

--- data-service/src/main/java/ru/nabokovsg/dataservice/dto/building/BuildingDto.java
+++ data-service/src/main/java/ru/nabokovsg/dataservice/dto/building/BuildingDto.java
@@ -4,8 +4,8 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
-import ru.nabokovsg.dataservice.dto.address.AddressDto;
 import ru.nabokovsg.dataservice.dto.department.ShortDepartmentDto;
+import ru.nabokovsg.dataservice.dto.requisites.RequisitesDto;
 
 @Setter
 @Getter
@@ -14,12 +14,12 @@ public class BuildingDto {
 
     @Schema(description = "Индентификатор")
     private Long id;
-    @Schema(description = "Адрес")
-    private AddressDto address;
     @Schema(description = "Тип строения")
     private String buildingType;
     @Schema(description = "Название")
     private String login;
     @Schema(description = "Подразделение филиала организации")
     private ShortDepartmentDto department;
+    @Schema(description = "Реквизиты")
+    private RequisitesDto requisites;
 }
