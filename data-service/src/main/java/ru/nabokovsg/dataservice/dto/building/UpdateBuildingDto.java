f36a4e9a6952d1ef1d5c56d50c8935edb8b78b03
NabokovSG <nabokovsg@yandex.ru>
2023-07-24 14:29:13 +0300

    add field requisites

--- data-service/src/main/java/ru/nabokovsg/dataservice/dto/building/UpdateBuildingDto.java
+++ data-service/src/main/java/ru/nabokovsg/dataservice/dto/building/UpdateBuildingDto.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import ru.nabokovsg.dataservice.dto.requisites.UpdateRequisitesDto;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
@@ -18,10 +19,6 @@ public class UpdateBuildingDto {
     @NotNull(message = "id tank passport should not be blank")
     @Positive(message = "id tank passport must be positive")
     private Long id;
-    @Schema(description = "Индентификатор адреса")
-    @NotNull(message = "address id should not be blank")
-    @Positive(message = "address id can only be positive")
-    private Long addressId;
     @Schema(description = "Тип строения")
     @NotBlank(message = "building should not be blank")
     private String buildingType;
@@ -31,4 +28,6 @@ public class UpdateBuildingDto {
     @NotNull(message = "department id should not be blank")
     @Positive(message = "department id can only be positive")
     private Long departmentId;
+    @Schema(description = "Реквизиты")
+    private UpdateRequisitesDto requisites;
 }
