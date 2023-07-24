fcf479415df83ac6fabab5ad06517055c83f921f
NabokovSG <nabokovsg@yandex.ru>
2023-07-24 14:32:19 +0300

    add field address

--- data-service/src/main/java/ru/nabokovsg/dataservice/dto/requisites/RequisitesDto.java
+++ data-service/src/main/java/ru/nabokovsg/dataservice/dto/requisites/RequisitesDto.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import ru.nabokovsg.dataservice.dto.address.AddressDto;
 
 @Setter
 @Getter
@@ -13,6 +14,8 @@ public class RequisitesDto {
 
     @Schema(description = "Индентификатор")
     private Long id;
+    @Schema(description = "Адрес организации")
+    private AddressDto address;
     @Schema(description = "Почтовый индекс")
     private Integer index;
     @Schema(description = "Номер телефона")
