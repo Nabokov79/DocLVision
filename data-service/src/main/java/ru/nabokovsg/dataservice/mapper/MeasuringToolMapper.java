6d38098fca66d09ef4431364be1ed6a896699498
NabokovSG <nabokovsg@yandex.ru>
2023-07-27 16:57:59 +0300

    delete methods mapToNeMeasuringToolValue, mapToUpdateMeasuringToolValue

--- data-service/src/main/java/ru/nabokovsg/dataservice/mapper/MeasuringToolMapper.java
+++ data-service/src/main/java/ru/nabokovsg/dataservice/mapper/MeasuringToolMapper.java
@@ -2,7 +2,6 @@ package ru.nabokovsg.dataservice.mapper;
 
 import org.mapstruct.Mapper;
 import ru.nabokovsg.dataservice.dto.measuringTool.MeasuringToolDto;
-import ru.nabokovsg.dataservice.dto.measuringTool.MeasuringToolIds;
 import ru.nabokovsg.dataservice.dto.measuringTool.NewMeasuringToolDto;
 import ru.nabokovsg.dataservice.dto.measuringTool.UpdateMeasuringToolDto;
 import ru.nabokovsg.dataservice.model.MeasuringTool;
@@ -14,12 +13,9 @@ public interface MeasuringToolMapper {
 
     MeasuringTool mapToNewMeasuringTool(NewMeasuringToolDto newMeasuringToolDto);
 
-    MeasuringToolDto mapToMeasuringToolDto(MeasuringTool measuringTools);
+    List<MeasuringToolDto> mapToMeasuringToolDto(List<MeasuringTool> measuringTools);
 
     MeasuringTool mapToUpdateMeasuringTool(UpdateMeasuringToolDto measuringToolDto);
 
     List<MeasuringToolDto> mapToMeasuringToolsDto(List<MeasuringTool> newMeasuringTools);
-
-    MeasuringToolIds mapToNeMeasuringToolValue(NewMeasuringToolDto newMeasuringToolDto);
-    MeasuringToolIds mapToUpdateMeasuringToolValue(UpdateMeasuringToolDto measuringToolDto);
 }
