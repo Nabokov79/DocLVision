3faad1e48558f2bbbe7cecd8b126f114d6fc213a
Nabokov79 <nabokovsg@yandex.ru>
2023-07-28 21:44:08 +0300

    add methods: mapToNewRequisitesDto, mapToUpdateRequisitesDto

--- data-service/src/main/java/ru/nabokovsg/dataservice/mapper/RequisitesMapper.java
+++ data-service/src/main/java/ru/nabokovsg/dataservice/mapper/RequisitesMapper.java
@@ -2,7 +2,9 @@ package ru.nabokovsg.dataservice.mapper;
 
 import org.mapstruct.Mapper;
 import ru.nabokovsg.dataservice.dto.requisites.NewRequisitesDto;
+import ru.nabokovsg.dataservice.dto.requisites.NewRequisitesEmployeeDto;
 import ru.nabokovsg.dataservice.dto.requisites.UpdateRequisitesDto;
+import ru.nabokovsg.dataservice.dto.requisites.UpdateRequisitesEmployeeDto;
 import ru.nabokovsg.dataservice.model.Requisites;
 
 @Mapper(componentModel = "spring")
@@ -11,4 +13,8 @@ public interface RequisitesMapper {
     Requisites mapToNewRequisites(NewRequisitesDto requisitesDto);
 
     Requisites mapToUpdateRequisites(UpdateRequisitesDto requisitesDto);
+
+    NewRequisitesDto mapToNewRequisitesDto(NewRequisitesEmployeeDto requisitesDto);
+
+    UpdateRequisitesDto mapToUpdateRequisitesDto(UpdateRequisitesEmployeeDto requisitesDto);
 }
