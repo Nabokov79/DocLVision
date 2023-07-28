429708537b7e3374b72f3c8b7f992d6295de2ccb
Nabokov79 <nabokovsg@yandex.ru>
2023-07-28 21:47:06 +0300

    replace  field type requisites UpdateRequisitesDt -> UpdateRequisitesEmployeeDto

--- data-service/src/main/java/ru/nabokovsg/dataservice/dto/employee/UpdateEmployeeDto.java
+++ data-service/src/main/java/ru/nabokovsg/dataservice/dto/employee/UpdateEmployeeDto.java
@@ -4,7 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
-import ru.nabokovsg.dataservice.dto.requisites.UpdateRequisitesDto;
+import ru.nabokovsg.dataservice.dto.requisites.UpdateRequisitesEmployeeDto;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
@@ -33,7 +33,7 @@ public class UpdateEmployeeDto {
     @NotBlank(message = "post should not be blank")
     private String post;
     @Schema(description = "Реквизиты сотрудника")
-    private UpdateRequisitesDto requisites;
+    private UpdateRequisitesEmployeeDto requisites;
     @Schema(description = "Индентификатор организации")
     @NotNull(message = "organization id user should not be blank")
     @Positive(message = "organization id user must be positive")
