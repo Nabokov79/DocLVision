4f9acd5e81c39481c7523604d57fba7035681d5b
NabokovSG <nabokovsg@yandex.ru>
2023-07-28 15:27:52 +0300

    add dto class

---
+++ data-service/src/main/java/ru/nabokovsg/dataservice/dto/ObjectsIds.java
@@ -0,0 +1,19 @@
+package ru.nabokovsg.dataservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ObjectsIds {
+
+    private Long controlTypeId;
+    private Long organizationId;
+    private Long employeeId;
+    private Long toolOwnerId;
+    private Long manufacturerId;
+}
