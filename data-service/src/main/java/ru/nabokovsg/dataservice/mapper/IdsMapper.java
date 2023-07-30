f73db96eb3e08f0123f751ff5396148cf48cf413
Nabokov79 <nabokovsg@yandex.ru>
2023-07-30 16:04:52 +0300

    add methods: mapFromNewObjectDto, mapFromUpdateObjectDto, mapFromNewLicense, mapFromUpdateLicense

--- data-service/src/main/java/ru/nabokovsg/dataservice/mapper/IdsMapper.java
+++ data-service/src/main/java/ru/nabokovsg/dataservice/mapper/IdsMapper.java
@@ -4,8 +4,12 @@ import org.mapstruct.Mapper;
 import ru.nabokovsg.dataservice.dto.ObjectsIds;
 import ru.nabokovsg.dataservice.dto.certificate.NewCertificateDto;
 import ru.nabokovsg.dataservice.dto.certificate.UpdateCertificateDto;
+import ru.nabokovsg.dataservice.dto.license.NewLicenseDto;
+import ru.nabokovsg.dataservice.dto.license.UpdateLicenseDto;
 import ru.nabokovsg.dataservice.dto.measuringTool.NewMeasuringToolDto;
 import ru.nabokovsg.dataservice.dto.measuringTool.UpdateMeasuringToolDto;
+import ru.nabokovsg.dataservice.dto.object.NewObjectDto;
+import ru.nabokovsg.dataservice.dto.object.UpdateObjectDto;
 
 @Mapper(componentModel = "spring")
 public interface IdsMapper {
@@ -17,4 +21,12 @@ public interface IdsMapper {
     ObjectsIds mapFromNewMeasuringToolDto(NewMeasuringToolDto measuringToolDto);
 
     ObjectsIds mapFromUpdateMeasuringToolDto(UpdateMeasuringToolDto measuringToolDto);
+
+    ObjectsIds mapFromNewObjectDto(NewObjectDto objectDto);
+
+    ObjectsIds mapFromUpdateObjectDto(UpdateObjectDto objectDto);
+
+    ObjectsIds mapFromNewLicense(NewLicenseDto objectDto);
+
+    ObjectsIds mapFromUpdateLicense(UpdateLicenseDto objectDto);
 }
