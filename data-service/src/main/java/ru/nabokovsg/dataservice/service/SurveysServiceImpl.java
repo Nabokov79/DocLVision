c0710d6c7a37f33e5cdbe2fe271160b0aae8e439
NabokovSG <nabokovsg@yandex.ru>
2023-08-03 10:19:24 +0300

    update methods save, update

--- data-service/src/main/java/ru/nabokovsg/dataservice/service/SurveysServiceImpl.java
+++ data-service/src/main/java/ru/nabokovsg/dataservice/service/SurveysServiceImpl.java
@@ -2,6 +2,7 @@ package ru.nabokovsg.dataservice.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import ru.nabokovsg.dataservice.dto.Builder;
 import ru.nabokovsg.dataservice.dto.ObjectsIds;
 import ru.nabokovsg.dataservice.dto.survey.NewSurveyDto;
 import ru.nabokovsg.dataservice.dto.survey.SurveyDto;
@@ -10,8 +11,6 @@ import ru.nabokovsg.dataservice.mapper.IdsMapper;
 import ru.nabokovsg.dataservice.mapper.SurveysMapper;
 
 import ru.nabokovsg.dataservice.model.BuilderType;
-import ru.nabokovsg.dataservice.model.Objects;
-import ru.nabokovsg.dataservice.model.Organization;
 import ru.nabokovsg.dataservice.model.Survey;
 import ru.nabokovsg.dataservice.repository.SurveysRepository;
 
@@ -19,7 +18,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -27,27 +25,21 @@ public class SurveysServiceImpl implements SurveysService {
 
     private final SurveysRepository repository;
     private final SurveysMapper mapper;
-    private final ObjectsService objectsService;
     private final BuilderService builderService;
     private final IdsMapper idsMapper;
 
     @Override
     public List<SurveyDto> save(List<NewSurveyDto> surveysDto) {
-        Map<Long, Objects> objects = getObjects(surveysDto.stream()
-                                                          .map(NewSurveyDto::getObjectId)
-                                                          .toList());
-        Map<Long, Organization> organizations = getOrganizations(surveysDto.stream()
-                                                                           .map(idsMapper::mapFromNewSurvey)
-                                                                           .toList());
+        Builder builder = getObjectsAndOrganization(surveysDto.stream().map(idsMapper::mapFromNewSurveyDto).toList());
         Map<Long, List<Survey>> surveyDb = new HashMap<>();
-        for (Long objectId : objects.keySet()) {
+        for (Long objectId : builder.getObjects().keySet()) {
             if (surveyDb.get(objectId) == null) {
                 List<Survey> surveys = new ArrayList<>();
                 for (NewSurveyDto surveyDto : surveysDto.stream()
                                                         .filter(s -> s.getObjectId().equals(objectId))
                                                         .toList()) {
                     Survey survey = mapper.mapToNewSurvey(surveyDto);
-                    survey.setOrganization(organizations.get(objectId));
+                    survey.setOrganization(builder.getOrganizations().get(objectId));
                     surveys.add(survey);
                 }
                 surveyDb.put(objectId, repository.saveAll(surveys));
@@ -57,43 +49,18 @@ public class SurveysServiceImpl implements SurveysService {
         return null;
     }
 
-//    @Override
-//    public List<SurveyDto> save(List<NewSurveyDto> surveysDto) {
-//        Map<Long, Objects> objects = objectsService.getAllById(surveysDto.stream()
-//                        .map(NewSurveyDto::getObjectId)
-//                        .toList())
-//                .stream()
-//                .collect(Collectors.toMap(Objects::getId, o -> o));
-//        Map<Long, Organization> organizations = builderService.getBuilder(surveysDto
-//                        .stream()
-//                        .map(idsMapper::mapFromNewSurvey).toList(), BuilderType.LICENSE)
-//                .getOrganizations();
-//        List<Survey> surveys = new ArrayList<>();
-//        for (NewSurveyDto surveyDto : surveysDto) {
-//            Survey survey = mapper.mapToNewSurvey(surveyDto);
-//            survey.setOrganization(organizations.get(surveyDto.getOrganizationId()));
-//            surveys.add(survey);
-//        }
-//        return mapper.mapToSurveyDto(repository.saveAll(surveys));
-//    }
-
     @Override
     public List<SurveyDto> update(List<UpdateSurveyDto> surveysDto) {
-        Map<Long, Objects> objects = getObjects(surveysDto.stream()
-                                                          .map(UpdateSurveyDto::getObjectId)
-                                                          .toList());
-        Map<Long, Organization> organizations = getOrganizations(surveysDto.stream()
-                                                                           .map(idsMapper::mapFromUpdateSurvey)
-                                                                           .toList());
+        Builder builder = getObjectsAndOrganization(surveysDto.stream().map(idsMapper::mapFromUpdateSurveyDto).toList());
         Map<Long, List<Survey>> surveyDb = new HashMap<>();
-        for (Long objectId : objects.keySet()) {
+        for (Long objectId : builder.getObjects().keySet()) {
             if (surveyDb.get(objectId) == null) {
                 List<Survey> surveys = new ArrayList<>();
                 for (UpdateSurveyDto surveyDto : surveysDto.stream()
                         .filter(s -> s.getObjectId().equals(objectId))
                         .toList()) {
                     Survey survey = mapper.mapToUpdateSurvey(surveyDto);
-                    survey.setOrganization(organizations.get(objectId));
+                    survey.setOrganization(builder.getOrganizations().get(objectId));
                     surveys.add(survey);
                 }
                 surveyDb.put(objectId, repository.saveAll(surveys));
@@ -103,12 +70,7 @@ public class SurveysServiceImpl implements SurveysService {
         return null;
     }
 
-    private Map<Long, Objects> getObjects(List<Long> ids) {
-        return objectsService.getAllById(ids).stream()
-                                             .collect(Collectors.toMap(Objects::getId, o -> o));
-    }
-
-    private Map<Long, Organization> getOrganizations(List<ObjectsIds> ids) {
-        return builderService.getBuilder(ids, BuilderType.LICENSE).getOrganizations();
+    private Builder getObjectsAndOrganization(List<ObjectsIds> ids) {
+        return builderService.getBuilder(ids, BuilderType.SURVEY);
     }
 }
