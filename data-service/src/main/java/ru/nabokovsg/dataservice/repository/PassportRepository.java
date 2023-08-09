86d8d3b0bc2d65b0f708282375ee29e5629b4579
Nabokov79 <nabokovsg@yandex.ru>
2023-08-09 20:42:57 +0300

    without changes

--- data-service/src/main/java/ru/nabokovsg/dataservice/repository/PassportRepository.java
+++ data-service/src/main/java/ru/nabokovsg/dataservice/repository/PassportRepository.java
@@ -3,6 +3,5 @@ package ru.nabokovsg.dataservice.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.nabokovsg.dataservice.model.Passport;
 
-
 public interface PassportRepository extends JpaRepository<Passport, Long> {
 }
